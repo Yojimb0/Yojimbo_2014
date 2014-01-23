a9764d2cb733b8fbb3380851f455fc5bd8f6ec52
Yojimbo <pypointereau+dev@gmail.com>
2014-01-24 00:09:48 +1100

    Full structure rework

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,58 +1,143 @@
 $(function(){
-	var $win = $(window),
-		$p = $('.page'),
-		$m = $('.main'),
-		$w = $('.w'),
-		$d = $('.d'),
-		$sa = $('.scrollable_area'),
+
+	/* SELECTORS & VARIABLES
+	---------------------------------------*/
+	var $win   = $(window),
+		$body  = $('body'),
+		$page  = $('.page', $body),
+		$main  = $('.main', $page),
+		$w     = $('.w', $page),
+		$d     = $('.d', $page),
+		$sa    = $('.scrollable_area', $page),
+		$paths = $('.c path', $main),
+		smallScreen = false,
 		scrollWidth = null,
-		mobile = ( $win.width() <= 855 ) ? true : false;
+		breakPoint = 855;
 
-	// ENABLE TRANSITIONS AFTER LOAD
 	
-	$(window).load(function() {
-		scrollWidth = scrollbarWidth();
-		updateScrollArea($sa, scrollWidth);
-		$('body').removeClass('disable_transitions');
-		animateSvg();
-	});
+	/* METHODS & HELPERS
+	---------------------------------------*/
+	// updateScrollAreas
+	// Let us recalculate width of scrollable element, in order to hide the scrollbar
+	function updateScrollAreas(){
+		$sa.each(function(){
+			var $this = $(this);
+			$this.width($this.parent().width() + scrollWidth);
+		});
+	}
 
-	// TODO
-	$( window ).resize(function() {
-		console.log('window.resize()');
-		updateScrollArea($sa, scrollWidth);
-	});
+	// updateResponsiveBehaviour
+	// Technique to get the responsive state (smallScreen/desktop) in the Javascript
+	// In order to bind/unbind the click event on the <aside> buttons
+	function updateResponsiveBehaviour(){
+		var cssDisplay = $('#responsive_state').css('display');
 
-	// CLICK ASIDES
-	if(mobile) $p.on('click', '.aside_button', function(){
-		var self = this,
-			$self = $(this),
-			$aside = $self.closest('aside'),
-			$asides = $p.find('aside'),
-			c = 'aside_open';
-
-		if($aside.hasClass(c)){
-			$asides.removeClass(c);
+		if( cssDisplay === 'none'){
+			if(smallScreen === false) $body.on('click.aside_button', '.aside_button', asidesSwap);
+			smallScreen = true;
 		}else{
-			$asides.removeClass(c);
-			$aside.addClass(c);
+			$body.off('click.aside_button');
+			smallScreen = false;
 		}
-	});
+	}
+
+	// scrollbarWidth
+	// Calculate scrollbar width, with the help ok 2 div hidden in the CSS
+	function scrollbarWidth() {
+		var $sbm  = $('#scrollbar_measure'),
+			wParent   = $sbm.width(),
+			wChildren = $('div', $sbm).width();
+		return (wParent - wChildren);
+	}
+
+	// asidesSwap
+	// Displaying / hiding asides panels, only in smallScreen behaviour
+	function asidesSwap(){
+		var $this = $(this),
+			$aside = $this.closest('aside'),
+			$asides = $('aside', $page),
+			openClass = 'aside_open';
+		console.log('Swapping <aside>', $this);
+
+		if($aside.hasClass(openClass)){
+			$asides.removeClass(openClass);
+		}else{
+			$asides.removeClass(openClass);
+			$aside.addClass(openClass);
+		}
+	}
+
+	// openModal
+	// Opens a project popup
+	function openModal(){
+		var $this = $(this);
+		$('#project_'+$this.data('project'), $body).addClass('project_show');
+	}
+
+	// closeModal
+	// Closes a project popup
+	function closeMmodal(){
+		var $this = $(this);
+		$('.project_show', $body).removeClass('project_show');
+	}
+
+	// animateSvg
+	// Animate a fake drawing of the main SVG avatar at the site loading
+	function animateSvg(){
+		for(var i = 0; i < $paths.length; ++i) {
+			var path = $paths[i];
+			var length = path.getTotalLength();
+			// Clear any previous transition
+			path.style.transition = path.style.WebkitTransition = 'none';
+			// Set up the starting positions
+			path.style.strokeDasharray = length + ' ' + length;
+			path.style.strokeDashoffset = length;
+			path.style.stroke = "#333";
+			path.style.strokeWidth = "2";
+			path.style.strokeOpacity = "1";
+			path.style.fillOpacity = "0";
+			// Trigger a layout so styles are calculated & the browser
+			// picks up the starting position before animating
+			path.getBoundingClientRect();
+			// Define our transition
+			path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out, stroke-opacity 2s ease-in-out, fill-opacity 2s ease-in-out 0.5s';
+			// Go!
+			path.style.strokeDashoffset = '0';
+			path.style.strokeOpacity = '0';
+			path.style.fillOpacity = '1';
+		}
+	}
+
+	// init
+	// Initialisation function, run once on page load
+	function init(){
+		// Get the width of scrollbars
+		scrollWidth = scrollbarWidth();
+		// Update the scrollAreas (to hide the scrollbars, depending on their width)
+		updateScrollAreas();
+		// Check the responsive state (smallScreen/desktop)
+		updateResponsiveBehaviour();
+		// Reactivate css transmission
+		$('body').removeClass('disable_transitions');
+		// Start the SVG animation
+		animateSvg();
+	}
+
+	// updateOnResize
+	// Function run at every resize event, in order to recalculate scroll areas, and update responsive state (JS side)
+	function updateOnResize(){
+		updateScrollAreas();
+		updateResponsiveBehaviour();
+	}
 	
 
-	$('.w').on('click', '.project_link', function(e){
-		var self = this, $self = $(this);
-		$('#project_'+$self.data('project')).addClass('project_show');
-	});
-
-	$('.project_close, .overlay').on('click', function(){
-		var self = this, $self = $(this);
-		$('.project_show').removeClass('project_show');
-	});
-
-
-	
-	
+	/* EVENTS
+	---------------------------------------*/
+	$win.load(init);
+	$win.resize(updateOnResize);
+	$w.on('click', '.project_link', openModal);
+	$('.project_close', $body).on('click', closeMmodal);
+	$('.overlay', $body).on('click', closeMmodal);
 
 
 
@@ -60,43 +145,5 @@ $(function(){
 });
 
 
-function animateSvg(){
-	var paths = document.querySelectorAll('.c path');
-	for(var i = 0; i < paths.length; ++i) {
-		var path = paths[i];
-		var length = path.getTotalLength();
-		// Clear any previous transition
-		path.style.transition = path.style.WebkitTransition = 'none';
-		// Set up the starting positions
-		path.style.strokeDasharray = length + ' ' + length;
-		path.style.strokeDashoffset = length;
-		path.style.stroke = "#333";
-		path.style.strokeWidth = "2";
-		path.style.strokeOpacity = "1";
-		path.style.fillOpacity = "0";
-		// Trigger a layout so styles are calculated & the browser
-		// picks up the starting position before animating
-		path.getBoundingClientRect();
-		// Define our transition
-		path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out, stroke-opacity 2s ease-in-out, fill-opacity 2s ease-in-out 0.5s';
-		// Go!
-		path.style.strokeDashoffset = '0';
-		path.style.strokeOpacity = '0';
-		path.style.fillOpacity = '1';
-	}
-}
 
-function scrollbarWidth() {
-	var w = $('.scrollbar_measure div').width();
-	return (50 - w);
-}
 
-function updateScrollArea($scrollAreas, scrollWidth){
-	// console.log('updateScrollArea() sw=', scrollWidth);
-	// console.log('updateScrollArea() sa=', $scrollAreas);
-	$scrollAreas.each(function(){
-		var self = this, $self = $(this);
-		$self.width($self.parent().width() + scrollWidth);
-		console.log('Was ',$self.width(),'Has to be',$self.width() + scrollWidth);
-	});
-}
